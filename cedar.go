cb425936739a92317a81eee895a73061c74ad81c
rsiddharth <s@ricketyspace.net>
2020-09-14 19:33:11 -0400

    cedar.go: update newsFeed
    Return []byte instead of string.

--- cedar.go
+++ cedar.go
@@ -5,28 +5,28 @@ import (
 	"net/http"
 )
 
-func newsFeed() (string, error) {
-	resp, err := http.Get("https://fsf.org.in/news/feed.atom")
-	if err != nil {
-		return "", err
-	}
-
-	// Init vars.
-	chunk := make([]byte, 100)
+func newsFeed() ([]byte, error) {
+	// Init feed.
 	feed := make([]byte, 0)
 
+	resp, err := http.Get("https://fsf.org.in/news/feed.atom")
+	if err != nil {
+		return feed, err
+	}
+
 	// Read feed.
+	chunk := make([]byte, 100)
 	for {
 		c, err := resp.Body.Read(chunk)
 		if c < 1 {
 			break
 		}
 		if err != nil && err != io.EOF {
-			return "", err
+			return feed, err
 		}
 		feed = append(feed, chunk[0:c]...)
 	}
-	return string(feed), nil
+	return feed, nil
 }
 
 func main() {}
